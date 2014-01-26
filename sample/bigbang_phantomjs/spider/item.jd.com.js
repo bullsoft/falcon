74118058048e7d2e12cd3fb2c819ab270dd805eb
Gu Weigang <cnwggu@gmail.com>
2014-01-27 03:59:33 +0800

    add phantomjs and change product config

---
+++ sample/bigbang_phantomjs/spider/item.jd.com.js
@@ -0,0 +1,41 @@
+module.exports = function (url, cb) {
+    var page = require('webpage').create(); 
+    page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.76 Safari/537.36';
+
+    page.onResourceRequested = function(request, network) {
+	if (request.url.indexOf('.css')     !== -1
+	    || request.url.indexOf('.png')  !== -1
+	    || request.url.indexOf('.jpg')  !== -1
+	    || request.url.indexOf('.jpeg') !== -1
+            || request.url.indexOf('.otf')  !== -1
+	    || request.url.indexOf('.swf')  !== -1
+	   ) {
+	    // console.log("abort " + request.url);
+	    network.abort();
+	}
+    };
+
+    page.onResourceReceived = function(response) {};
+    
+    page.open(url, function(status) {
+    	if (status !== 'success') {
+    	    console.log('Error: Unable to access network!');
+    	} else {
+    	    var data = page.evaluate(function() {
+    		var json_data = {};
+    		json_data.price = document.getElementById('jd-price').textContent;
+    		json_data.name = document.querySelector('div#name>h1:first-child').textContent;
+    		var img_url_arr = document.querySelectorAll('#spec-list li img');
+    		json_data.imgs = [];
+    		for(var i in img_url_arr){
+    		    if(img_url_arr[i].nodeType && (img_url_arr[i].nodeType == 1)){
+    			json_data.imgs.push(img_url_arr[i].src); 
+    		    }
+    		}
+    		return json_data;
+    	    });
+    	    console.log(JSON.stringify(data));
+    	}
+	cb();
+    });
+}
