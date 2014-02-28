1d0e281a928f90be80fa4029dc75ec4824fa1bf7
xiejunfeng <lovers80@126.com>
2014-02-28 23:11:04 +0800

    update
    update

--- sample/views/comment/list.volt
+++ sample/views/comment/list.volt
@@ -4,15 +4,23 @@
 		<a href="#"><img src="{{ reply.user.photo }}"/></a>
 	</div>
 	<div class="info content">
-
-		<a class="user-nick transition-all" href="#">{{ reply.user.nickname }}</a>
-		{% if reply.reply_to_user_id > 0 %}
-		回复 {{reply.replyto.nickname}}
-		{% endif %}
-		<span class="respond-text">{{reply.content}}</span>
-		<p class="time">
-			{{ reply.addtime }}
-		</p>
+		<div>
+			<a class="user-nick transition-all" href="#">{{ reply.user.nickname }}</a>
+			{% if reply.reply_to_user_id > 0 %}
+			回复 {{reply.replyto.nickname}}
+			{% endif %}
+			<span class="respond-text">{{reply.content}}</span>
+		</div>
+		<div class="clearer">
+			<span class="time"> {{ reply.addtime }} </span>
+			<a href="#" class="reply-to">回应</a>
+		</div>
+	</div>
+	<div class="respond-to-user">
+		<textarea class="normal-textarea"></textarea>
+		<button class="ck-btn doRespond" data-productid="{{product.id}}" data-id="{{comment.id}}" data-userid="{{reply.reply_to_user_id}}">
+			发布
+		</button>
 	</div>
 </div>
 {% endfor %}
--- skeleton/public/js/goods/comment.js
+++ skeleton/public/js/goods/comment.js
@@ -1,192 +1,205 @@
-(function($, win, doc){
-    
+(function($, win, doc) {
+
     var Comment;
-    
-    Comment = function(elment, options){
-        
-        var self  = this;
-        
-        
-        self.options = $.extend({},options,self.options,true);
+
+    Comment = function(elment, options) {
+
+        var self = this;
+
+        self.options = $.extend({}, options, self.options, true);
         self.$container = $(elment);
-        
+
         self.init();
     };
-    
-    $.extend(Comment.prototype,{
-        
+
+    $.extend(Comment.prototype, {
+
         options : {
             showRespondClass : ".showRespond",
             doRespondClass : '.doRespond',
-            respondIdPre: '#respond-list-',
-            respondBoxPre: '#respond-list-box-',
-            el:''
+            respondIdPre : '#respond-list-',
+            respondBoxPre : '#respond-list-box-',
+            respondBoxClass : '.respond-el',
+            showRespondToClass : '.reply-to',
+            respondToBoxClass : '.respond-to-user',
+            el : ''
         },
-        
-        showRespond : function(element, options){
-            
-            var self = this,
-                $list = $(self.options.respondIdPre + options.id);
+
+        showRespond : function(element, options) {
+
+            var self = this, $list = $(self.options.respondIdPre + options.id);
 
             $.ajax({
                 url : global.config.comment.showRespondUrl,
-                type: global.config.requestType,
-                data: {'comment_id': options.id, 'product_id':options.productId},
-                dataType: 'text',
-                success: function(data){
-                    
-                        
-                    $list.html(data).slideDown();
-                        
-                        
-                    $list.parent().find('.ajax-loading').hide();
-                    $(element).remove('ajax-doing');
-                    
+                type : global.config.requestType,
+                data : {
+                    'comment_id' : options.id,
+                    'product_id' : options.productId
                 },
-                error:function(data){
-                    
+                dataType : 'text',
+                success : function(data) {
+
+                    $list.html(data).slideDown();
+
+                    $list.parent().find('.ajax-loading').hide();
+                    $(element).removeClass('ajax-doing');
+
+                },
+                error : function(data) {
+
                     $list.parent().find('.ajax-loading').html('加载失败,请重新加载...');
-                    $(element).remove('ajax-doing');
+                    $(element).removeClass('ajax-doing');
                 }
             });
         },
-        
-        doRespond : function(element, options){
-            
-            var self = this, params={};
-            
+
+        doRespond : function(element, options) {
+
+            var self = this, params = {};
+
             params.comment = options.comment;
             params.product_id = options.productId;
             params.comment_id = options.id;
+            params.user_id = options.userId;
 
             $.ajax({
                 url : global.config.comment.doRespondUrl,
-                type: global.config.requestType,
-                data: params,
-                dataType: 'json',
-                success: function(res){
-                    
+                type : global.config.requestType,
+                data : params,
+                dataType : 'json',
+                success : function(res) {
+
                     var data = res.data;
-                    
-                    if(res.status == 200){
-                        
-                    }else if(res.status == 403){
-                        
+
+                    if (res.status == 200) {
+
+                    } else if (res.status == 403) {
+
                         new $.MSspirit.login($('body'), {}).showDialog();
-                        
-                    }else if(res.status == 500){
-                        
+
+                    } else if (res.status == 500) {
+
                         alert(res.msg);
                     }
-                    
-                    $(element).removeClass('.ajax-doing');
+
+                    $(element).removeClass('ajax-doing');
                 },
-                error:function(data){
-                    
-                    $(element).removeClass('.ajax-doing');
+                error : function(data) {
+
+                    $(element).removeClass('ajax-doing');
                     alert('网络错误');
                 }
             });
-             
+
         },
-        
-        bindEvents: function(){
-            
+
+        //事件注册
+        showReplyTo : function(element, event) {
+
+            var self = this, $el = $(element), $box;
+
+            $box = $el.parents(self.options.respondBoxClass).find(self.options.respondToBoxClass);
+
+            $box.toggle();
+
+        },
+
+        replyTo : function(element, event) {
+            var self = this, $el = $(element), id = $el.attr('data-id'),
+                productId = $el.attr('data-productid'), 
+                userId = $el.attr('data-userid'), val = $el.prev().val();
+
+            if ($.trim(val) == '') {
+                alert('请输入内容');
+                return;
+            }
+
+            if ($el.hasClass('ajax-doing')) {
+
+                return;
+            } else {
+                $el.addClass('ajax-doing');
+                self.doRespond($el, {
+                    'id' : id,
+                    productId : productId,
+                    userId : userId,
+                    comment : val
+                });
+            }
+
+        },
+
+        bindEvents : function() {
+
             var self = this;
-            
-            self.$container.on('click.doRespond.ms', self.options.doRespondClass, function(){
-                
-                var $this = $(this), $text, val,
-                    id = $this.attr('data-id'),
-                    productId =  $this.attr('data-productid');
-                    
-                $text = $this.prev();
-                val = $text.val();
-                
-                if($.trim(val) == ''){
-                    alert('请输入内容');
-                    return;
-                }
-                
-                if($this.hasClass('.ajax-doing')){
-                    
-                   return;
-                }else{
-                    
-                    self.doRespond($this, {'id':id, productId:productId,comment : val});
-                }
-                
+
+            self.$container.on('click.doRespond.ms', self.options.doRespondClass, function(event) {
+
+               self.replyTo(this, event);
+               return false;
+
             });
 
-            self.$container.on('click.showRespond.ms', self.options.showRespondClass, function(){
-                
-                var $this = $(this),  id = $this.attr('data-id'),  $box,
-                    $list, productId =  $this.attr('data-productid');
-                
+            self.$container.on('click.showRespond.ms', self.options.showRespondClass, function() {
+
+                var $this = $(this), id = $this.attr('data-id'), $box, $list, productId = $this.attr('data-productid');
+
                 $box = $(self.options.respondBoxPre + id);
                 $list = $(self.options.respondIdPre + id);
-                
-                
-                if($this.hasClass('open')){
-                    
+
+                if ($this.hasClass('open')) {
+
                     $box.slideUp();
-                    
+
                     $this.removeClass('open');
-                }else{
-                    
-                    
-                    if($.trim($list.html()) != ''){
-                        
+                } else {
+
+                    if ($.trim($list.html()) != '') {
+
                         $box.slideDown();
-                        
-                    }else{
-                        
-                        if($this.hasClass('ajax-doing')){return;}
-                        
+
+                    } else {
+
+                        if ($this.hasClass('ajax-doing')) {
+                            return;
+                        }
+
                         $this.addClass('ajax-doing');
-                        
+
                         $box.show();
-                        
+
                         $list.parent().find('.ajax-loading').html('评论正在加载中 ...').show();
-                        
+
                         self.showRespond($this, {
-                            id:id, 
-                            productId:productId
-                            
-                         });
+                            id : id,
+                            productId : productId
+
+                        });
                     }
-                    
+
                     $this.addClass('open');
                 }
-                
-                
-                
+
                 return false;
-                
+
             });
-            
+
+            self.$container.on('click.showReplyTo.ms', self.options.showRespondToClass, function(event) {
+                self.showReplyTo(this, event);
+                return false;
+            });
+
+
         },
-        
-        //初始化元素对象
-        initEl : function(){
-            
+
+        init : function() {
+
             var self = this;
-            
-           // self.$respond = self.$container.find('.'+self.options.showRespondClass);
-            
-        },
-        
-        
-        init: function(){
-            
-            var self = this;
-            
+
             self.bindEvents();
         }
-        
-        
     });
-    
+
     new Comment($('#goods-comment-list'));
-    
+
 })(jQuery, window, document);
